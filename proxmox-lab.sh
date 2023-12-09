5e11be7e418af49dec05a09446ee1e8187d4e931
William Bouzourène <william@bouzourene.ch>
2023-12-10 00:14:27 +0100

    Script for easy proxmox lab config

---
+++ proxmox-lab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Remove no subsucription message
+PROXMOXLIB="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+sed -Ez "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" $PROXMOXLIB
+systemctl restart pveproxy.service
+
+# Replace proxmox enterprise repo
+rm -f /etc/apt/sources.list.d/pve-enterprise.list
+NOSUBREPO="deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription"
+echo $NOSUBREPO > /etc/apt/sources.list.d/pve-no-subscription.list
+apt-get update

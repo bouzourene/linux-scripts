485d8d12e5257c15d9adaa58aeeb012e16110e23
William Bouzourène <william@bouzourene.ch>
2023-12-27 21:21:35 +0100

    Proxmox script: remove ceph repo & only patch js once

--- proxmox-lab.sh
+++ proxmox-lab.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # Remove no subsucription message
+# ===============================
+
 PROXMOXLIB="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
-sed -Ezi "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" $PROXMOXLIB
-systemctl restart pveproxy.service
+
+if grep -q "//Ext.Msg.show({" $PROXMOXLIB; then
+	echo "proxmoxlib.js -> no patching needed"
+else
+	echo "proxmoxlib.js -> patching needed"
+	sed -Ezi "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" $PROXMOXLIB
+	systemctl restart pveproxy.service
+fi
 
 # Replace proxmox enterprise repo
+# ===============================
+
 rm -f /etc/apt/sources.list.d/pve-enterprise.list
+rm -f /etc/apt/sources.list.d/ceph.list
+
 NOSUBREPO="deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription"
 echo $NOSUBREPO > /etc/apt/sources.list.d/pve-no-subscription.list
 apt-get update
